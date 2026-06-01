6b3c1aa0133ad433f3bd32bf7e259961f63a8dda
Evgeniy Gvozdikov <evgeniy.g@bnovo.ru>
2026-06-01 22:26:24 +0300

    chore(web): configure vite proxy

--- internal/presentation/http/web/vite.config.ts
+++ internal/presentation/http/web/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        proxy: {
+            '/api': {
+                target: 'http://lifeline-app:30000',
+                changeOrigin: true,
+            },
+        },
+    },
 })
